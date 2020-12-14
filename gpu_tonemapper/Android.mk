01c90be7eebbd27600de3063aad34de0b0e58796
Julian Veit <Claymore1298@gmail.com>
2020-12-14 17:01:24 +0100

    aa
    Change-Id: I5b452c1de5cd30c141106992bd9747acf02b834c

--- gpu_tonemapper/Android.mk
+++ gpu_tonemapper/Android.mk
@@ -2,6 +2,12 @@ LOCAL_PATH := $(call my-dir)
 include $(LOCAL_PATH)/../common.mk
 
 include $(CLEAR_VARS)
+LOCAL_COPY_HEADERS_TO     := $(common_header_export_path)
+LOCAL_COPY_HEADERS        := TonemapFactory.h Tonemapper.h
+LOCAL_VENDOR_MODULE       := true
+include $(BUILD_COPY_HEADERS)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE              := libgpu_tonemapper
 LOCAL_VENDOR_MODULE       := true
 LOCAL_MODULE_TAGS         := optional
@@ -11,7 +17,7 @@ LOCAL_SHARED_LIBRARIES    := libEGL libGLESv2 libGLESv3 libui libutils liblog
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 LOCAL_CFLAGS              := $(version_flag) -Wno-missing-field-initializers -Wall \
-                             -Wno-unused-parameter -DLOG_TAG=\"GPU_TONEMAPPER\"
+                             -Wno-unused-parameter -std=c++11 -DLOG_TAG=\"GPU_TONEMAPPER\"
 
 LOCAL_SRC_FILES           := TonemapFactory.cpp \
                              glengine.cpp \
